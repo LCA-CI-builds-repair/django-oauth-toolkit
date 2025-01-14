8d75ef90df3efe43bc5a76b69cc24a4b9b292f94
ubuntu <ubuntu@ip-10-0-1-31>
2025-01-14 15:37:43 +0000

    waleko__claude-3.5-sonnet_2172__id_184

--- oauth2_provider/settings.py
+++ oauth2_provider/settings.py
@@ -70,7 +70,7 @@ DEFAULTS = {
     "ALLOWED_REDIRECT_URI_SCHEMES": ["http", "https"],
     "ALLOWED_SCHEMES": ["https"],
     "OIDC_ENABLED": False,
-    "OIDC_ISS_ENDPOINT": "",
+    "OIDC_ISS_ENDPOINT": "/o",
     "OIDC_USERINFO_ENDPOINT": "",
     "OIDC_RSA_PRIVATE_KEY": "",
     "OIDC_RSA_PRIVATE_KEYS_INACTIVE": [],
